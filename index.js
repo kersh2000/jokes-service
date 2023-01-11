48c0416f557c52d5366fcf432ecba7175955b082
kersh2000 <bradleykershaw333@gmail.com>
2023-01-11 16:36:14 +0000

    Jokes API Exercise

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize')
 const app = express();
 const { Joke } = require('./db');
 
@@ -8,7 +9,29 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const tags = req.query["tags"] || ""
+    const content = req.query["content"] || ""
+    let jokes = await Joke.findAll({
+      where: {
+        joke: {
+          [Op.substring]: content
+        }
+      }
+    });
+
+    // Fetch jokes with matching content
+    let matches = []
+    jokes.forEach( joke => {
+      const jokeTags = joke["tags"].split(",")
+      if (jokeTags.indexOf(tags) !== -1){
+        matches.push(joke)
+      }
+    });
+
+    if (matches.length !== 0) {
+      jokes = matches
+    }
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
